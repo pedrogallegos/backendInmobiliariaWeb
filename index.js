a1e1f3bd558e5f441725eb67a1aee7c14e348172
pedrogallegos <pedrogallegos543@gmail.com>
2025-05-30 21:30:57 -0600

    Configuracion de backend en github

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!')
+    })
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+app.get('/respuesta', (req, res) => {
+    res.json({ message: 'Esta es una respuesta JSON' })
+})
